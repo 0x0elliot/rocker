22eddf169e210f924070c46b58c7fe885f207cb0
0x0elliot <wolfwithahat@protonmail.com>
2022-12-10 01:07:29 +0530

    Finishing up image utility.

--- src/image.rs
+++ src/image.rs
@@ -1,76 +1,34 @@
-use std::process::Command;
+use std::{fs::File, io::Write};
+use tar::Archive;
 
 const IMAGE_DIR: &str = concat!(env!("HOME"), "/.rocker", "/images");
+const FILE_PATH: &str = concat!(env!("HOME"), "/.rocker", "/images", "/filesystem.tar");
 
-// Right now, it removes all images.
-// This is a very experimental hack. Let it be for now.
-fn remove_image() {
-    let output = Command::new("rm")
-        .arg("-rf")
-        .arg(IMAGE_DIR)
-        .output()
-        .expect("failed to execute process");
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+fn download_image_tar() {
+    let url = "https://github.com/0x0elliot/rocker/blob/master/static/filesystem.tar?raw=true";
+    let resp = reqwest::blocking::get(url).unwrap();
+    let mut file = File::create(FILE_PATH).unwrap();
+    file.write_all(&resp.bytes().unwrap()).unwrap();
 }
 
-fn make_image_dir() {
-    let output = Command::new("mkdir")
-        .arg("-p")
-        .arg(IMAGE_DIR)
-        .output()
-        .expect("failed to execute process");
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+fn extract_image_tar() {
+    let mut ar = Archive::new(File::open(FILE_PATH).unwrap());
+    ar.unpack(IMAGE_DIR).unwrap();
 }
-
-fn docker_create_alpine() {
-    let output = Command::new("docker")
-        .arg("create")
-        .arg("alpine")
-        .output()
-        .expect("failed to execute process");
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-    String::from_utf8(output.stdout).unwrap();
-}
-
-fn docker_export_alpine() {
-    let image_hash = docker_create_alpine();
-    let output = Command::new("docker")
-        .arg("export")
-        .arg(image_hash)
-        .output()
-        .expect("failed to execute process");
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-    String::from_utf8_lossy(&output.stdout);
-}
-
-fn image_to_tar() {
-    let image = docker_export_alpine();
-    let output = Command::new("tar")
-        .arg("-C")
-        .arg(IMAGE_DIR)
-        .arg("-xvf")
-        .arg(image)
-        .output()
-        .expect("failed to execute process");
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-    String::from_utf8_lossy(&output.stdout);
-}
-
-
 
 // For the time being, ./rocker will hold one kind of image at once.
 // And everytime a new image is created, the old one is removed.
 pub fn handle_image_command() {
-    remove_image();
-    make_image_dir();
+    match std::fs::remove_dir_all(IMAGE_DIR) {
+        Ok(_) => println!("Removing existing image.."),
+        Err(_) => println!("Image didn't exist!"),
+    };
+    
+    match std::fs::create_dir(IMAGE_DIR) {
+        Ok(_) => println!("Creating new directory for it.."),
+        Err(_) => println!("Something went wrong. Expect errors."),
+    };
+
+    download_image_tar();
+    extract_image_tar();
 }
