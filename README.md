b2e4c0813ad1cd7361578198577e2920d5435513
0x0elliot <wolfwithahat@protonmail.com>
2022-12-10 03:29:57 +0530

    Adding a readme.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rocker
+
+A container runtime written in rust. Targetted to be an experimental child of my will to make something cool.
+
--- src/mount.rs
+++ src/mount.rs
@@ -1,3 +1,7 @@
+// fn mount_dir_bind() {
+//     nix::mount::mount::<str, str, str, str>(source, target, fstype, flags, data);
+// }
+
 pub fn handle_mount_command() {
-    println!("mount");
+    println!("mount");    
 }
